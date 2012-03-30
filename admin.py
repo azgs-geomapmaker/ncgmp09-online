a9997082a2870d73dd4bf08988a2400bb0644635
Ryan Clark <ryan.clark.j@gmail.com>
2012-03-30 16:28:41 -0700

    Built simple filtering ModelAdmin classes for the various models.

--- admin.py
+++ admin.py
@@ -1,5 +1,38 @@
 from django.contrib import admin
-from django.db.models import get_models
+from models import GeoMap, DataSources, Glossary, DescriptionOfMapUnits, MapUnitPolys, ContactsAndFaults, StandardLithology, RepresentativeValue, GeologicUnitView
 
-for cls in [ cls for cls in get_models() if cls._meta.app_label == "ncgmp" ]:
-    admin.site.register(cls)
+class GeoMapAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'title']
+admin.site.register(GeoMap, GeoMapAdmin)
+
+class DataSourcesAdmin(admin.ModelAdmin):
+    list_filter = ('owningmap',)
+admin.site.register(DataSources, DataSourcesAdmin)
+
+class GlossaryAdmin(admin.ModelAdmin):
+    list_filter = ('owningmap',)
+admin.site.register(Glossary, GlossaryAdmin)
+
+class DescriptionOfMapUnitsAdmin(admin.ModelAdmin):
+    list_filter = ('owningmap',)
+admin.site.register(DescriptionOfMapUnits, DescriptionOfMapUnitsAdmin)
+
+class MapUnitPolysAdmin(admin.ModelAdmin):
+    list_filter = ('owningmap',)
+admin.site.register(MapUnitPolys, MapUnitPolysAdmin)
+
+class ContactsAndFaultsAdmin(admin.ModelAdmin):
+    list_filter = ('owningmap',)
+admin.site.register(ContactsAndFaults, ContactsAndFaultsAdmin)
+
+class StandardLithologyAdmin(admin.ModelAdmin):
+    list_filter = ('owningmap',)
+admin.site.register(StandardLithology, StandardLithologyAdmin)
+
+class RepresentativeValueAdmin(admin.ModelAdmin):
+    list_filter = ('owningmap',)
+admin.site.register(RepresentativeValue, RepresentativeValueAdmin)
+
+class GeologicUnitViewAdmin(admin.ModelAdmin):
+    list_filter = ('owningmap',)
+admin.site.register(GeologicUnitView, GeologicUnitViewAdmin)
