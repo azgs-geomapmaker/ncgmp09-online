e8be3380309e52adf3741e45b1f5631aa69fb124
Ryan Clark <ryan.clark.j@gmail.com>
2012-03-29 17:04:14 -0700

    Some minor minor cleanups inside the layer loading loop

--- geomaps/dataloader.py
+++ geomaps/dataloader.py
@@ -26,20 +26,20 @@ class GdbLoader():
     def loadLayer(self, gdalLayer):
         cls = get_model("ncgmp", gdalLayer.name)
         clsFields = [ clsField for clsField in cls._meta.fields if clsField.name != "id" and clsField.name != "owningmap" ]
-        features = []
+        upperFields = [ field.upper() for field in gdalLayer.fields ]
         for feature in gdalLayer:
             newFeatureArgs = { "owningmap": self.geomap }                                                                           # New features are always related to the GeoMap being loaded
             for clsField in clsFields:                                                                                              # For each field in the destination table....
                 if clsField.name == "shape":                                                                                        # Field is the Shape / Geometry Field
                     newFeatureArgs[clsField.name] = feature.geom.geos                                                               # feature.geom.geos is a GEOSGeometry object, which can be passed as directly to the model                    
                 else:                                    
-                    gdalField = gdalLayer.fields[ [ field.upper() for field in gdalLayer.fields ].index(clsField.name.upper()) ]    # Find the appropriate field in the GDAL Layer                    
+                    gdalField = gdalLayer.fields[ upperFields.index(clsField.name.upper()) ]                                        # Find the appropriate field in the GDAL Layer                    
                     if isinstance(clsField, ForeignKey):                                                                            # If the field is a Foreign Key
                         relatedCls = clsField.rel.to                                                                                # Find the related Class                    
                         relatedFieldName = clsField.rel.field_name                                                                  # Find the field name in the related Class
                         relatedCriteria = { relatedFieldName: feature.get(gdalField) }                                              # Sets up the filter criteria to find the correct instance of the related Class
                         newFeatureArgs[clsField.name] = relatedCls.objects.get(**relatedCriteria)                                   # Get the related instance, add it to kwargs for the new feature
-                    elif clsField.name != "shape":                                                                                  # Field is not a Foreign Key, and is not a Shape field
+                    else:                                                                                                           # Field is not a Foreign Key, and is not a Shape field
                         newFeatureArgs[clsField.name] = feature.get(gdalField)                                                 
                 
             newFeature = cls(**newFeatureArgs)
