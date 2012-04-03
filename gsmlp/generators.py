ed52a7d51f1738ae14aa26525e448a21cad48c2a
Ryan Clark <ryan.clark.j@gmail.com>
2012-04-03 13:00:08 -0700

    Include representative values in gsmlp

--- gsmlp/generators.py
+++ gsmlp/generators.py
@@ -10,6 +10,7 @@ class GeologicUnitViewGenerator:
         
     def createGeologicUnitView(self, mapunitpoly):
         dmu = mapunitpoly.mapunit
+        repValues = dmu.representativevalue_set.all()
         ds = mapunitpoly.datasourceid
         
         kwargs = {
@@ -17,23 +18,27 @@ class GeologicUnitViewGenerator:
             "identifier": mapunitpoly.mapunitpolys_id,
             "name": dmu.name,
             "description": dmu.description,
-            "geologicUnitType": "none",
+            "geologicUnitType": "none",                     # What should this be?
             "rank": dmu.hierarchykey,
             "lithology": dmu.generallithologyterm,
-            "geologicHistory": "none",
-            "observationMethod": "none",
-            "positionalAccuracy": "none",
+            "geologicHistory": "none",                      # Where could this come from?
+            "observationMethod": "none",                    # Where could this come from?
+            "positionalAccuracy": "none",                   # Where could this come from?
             "source": ds.source,
-            "geologicUnitType_uri": "none",
-            "representativeLithology_uri": "none",
+            "geologicUnitType_uri": "none",                 # Where could this come from?
+            "representativeLithology_uri": "none",            
             "representativeAge_uri": "none",
-            "representativeLowerAge_uri": "none",
-            "representativeUpperAge_uri": "none",
-            "specification_uri": "none",
-            "metadata_uri": "none",
-            "genericSymbolizer": "none",
+            "representativeLowerAge_uri": "none",           # From a related GeologicEvent. Can there be more than one?
+            "representativeUpperAge_uri": "none",           # From a related GeologicEvent. Can there be more than one?
+            "specification_uri": "none",                    # What should this be?
+            "metadata_uri": "none",                         # Where could this come from?
+            "genericSymbolizer": "none",                    # Where could this come from?
             "shape": mapunitpoly.shape
         }
+        
+        if len(repValues) > 0: 
+            kwargs["representativeLithology_uri"] = repvalues[0].representativelithology_uri
+            kwargs["representativeAge_uri"] = repvalues[0].representativeage_uri
         
         GeologicUnitView = get_model("ncgmp", "GeologicUnitView")
         return GeologicUnitView(**kwargs)
